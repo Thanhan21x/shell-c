e4f448177614eddb24fe26027fa3a46e54ff446a
Thanh Nhan <thanh.nhan.21@proton.me>
2025-12-21 09:57:14 +0700

    echo/3rd try

--- src/main.c
+++ src/main.c
@@ -6,10 +6,10 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   
-  char command[1024];
   while (1) {
     printf("$ ");
 
+    char command[1024];
     fgets(command, sizeof(command), stdin);
 
     // Remove the trailing newline

8150c7b72ffe2dff243b4c113dd134792398c4e4
Thanh Nhan <thanh.nhan.21@proton.me>
2025-12-21 13:48:28 +0700

    type/done

--- src/main.c
+++ src/main.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_COMMAND_NUM 128
+#define MAX_COMMAND_LEN 32
+
+int is_builtin_command(const char* cmd) {
+  if (!strcmp(cmd, "exit") || 
+      !strcmp(cmd, "echo") ||
+      !strcmp(cmd, "type")) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -22,6 +36,13 @@ int main(int argc, char *argv[]) {
       char *msg = memchr(command, ' ', strlen(command)) + 1;
       printf("%s\n", msg);
 
+    } else if (strstr(command, "type")) {
+      char *cmd = memchr(arg_start, ' ', strlen(arg_start)) 
+      if (is_builtin_command(cmd)) {
+        printf("%s is a shell builtin\n", cmd);
+      } else {
+        printf("%s: not found\n", cmd);
+      }
     } else {
       printf("%s: command not found\n", command);
     }

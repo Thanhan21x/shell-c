a61e588c68896e454189967e2836df5d1ff1cbbc
Thanh Nhan <thanh.nhan.21@proton.me>
2025-12-21 09:52:01 +0700

    exit/first try

--- src/main.c
+++ src/main.c
@@ -15,6 +15,10 @@ int main(int argc, char *argv[]) {
     // Remove the trailing newline
     command[strcspn(command, "\n")] = '\0';
 
+    if (!strcmp(command, "exit")) {
+      break;
+    }
+
     printf("%s: command not found\n", command);
   }
 

ae7cce28a53db30c72350c6541cdc89f6932cdba
Thanh Nhan <thanh.nhan.21@proton.me>
2025-12-22 12:11:21 +0700

    PATH/ok

--- src/main.c
+++ src/main.c
@@ -1,18 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 #define MAX_COMMAND_NUM 128
 #define MAX_COMMAND_LEN 32
 
-int is_builtin_command(const char* cmd) {
-  if (!strcmp(cmd, "exit") || 
-      !strcmp(cmd, "echo") ||
-      !strcmp(cmd, "type")) {
+
+#define PATH "/usr/bin:/usr/local/bin"
+
+int is_builtin_command(const char *arg) {
+  if (!strcmp(arg, "exit") || 
+      !strcmp(arg, "echo") ||
+      !strcmp(arg, "type")) {
     return 1;
   } else {
     return 0;
   }
+}
+
+int is_executable_file(const char *path) {
+  struct stat st;
+
+  if (stat(path, &st) != 0)
+    return 0;
+
+  return (st.st_mode & S_IXUSR) != 0;
+}
+
+int is_file_in_path(const char* file, const char *path) {
+  // list all executable in path and compare it with the arg
+  DIR *dirp = opendir(path); 
+  if (!dirp) {
+    perror("opendir");
+    exit(1);
+  }
+  struct dirent *entry;
+
+
+  while (entry = readdir(dirp)) {
+    if (!strcmp(entry->d_name, file)) {
+      char full_path[256];
+      snprintf(full_path, sizeof(full_path), "%s/%s", path, file);
+      if (is_executable_file(full_path)){
+        return 1;
+      }
+    }
+  }
+
+  return 0;
+}
+
+int is_executable_command(const char *arg, char *path_out) {
+  char paths[256];
+  strcpy(paths, PATH);
+
+  char *ptr = paths;
+
+
+  char *end = memchr(paths, '\0', strlen(paths) + 1);
+  if (end == NULL) {
+    fprintf(stderr,"Wrong string format for PATH");
+    exit(1);
+  }
+
+
+  while (ptr < end) {
+    char *colon = memchr(ptr, ':', strlen(ptr) + 1);
+
+    char path[64];
+    if (colon) {
+      int path_len = colon - paths;
+      memcpy(path, ptr, path_len);
+      path[path_len] = '\0';
+      ptr += strlen(path) + 1;
+
+      if (is_file_in_path(arg, path)) {
+        strcpy(path_out, path);
+        return 1;
+      }
+
+    } else {
+      strcpy(path, ptr); // there is only one path, or its the last paths
+      ptr += strlen(path) + 1 ;
+
+      if (is_file_in_path(arg, path)) {
+        strcpy(path_out, path);
+        return 1;
+      }
+    }
+  }
+  // for every paths in PATH:
+  //  check if paths/arg is in there
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -31,6 +112,9 @@ int main(int argc, char *argv[]) {
     // Take the command which is the word before the first space
     char command[64];
     char *space_pos = memchr(input, ' ', strlen(input));
+
+    char path_found[64];
+
     if (space_pos == NULL) {
       // only one argument
       strcpy(command, input);
@@ -38,8 +122,6 @@ int main(int argc, char *argv[]) {
       int len = space_pos - input;
       memcpy(command, input, len);
       command[len] = '\0';
-
-
     }
        if (!strcmp(command, "exit")) {
       break;
@@ -50,9 +132,14 @@ int main(int argc, char *argv[]) {
 
     } else if (!strcmp(command, "type")) {
       char *cmd = memchr(input, ' ', strlen(input)) + 1;
+      // Check if it's a builtin command
       if (is_builtin_command(cmd)) {
         printf("%s is a shell builtin\n", cmd);
-      } else {
+      // Check if it's an executable, print its path if found
+      } else if (is_executable_command(cmd, path_found)) {
+        // copy the executable's path to path
+        printf("%s is %s/%s\n", cmd, path_found, cmd);
+      }else {
         printf("%s: not found\n", cmd);
       }
     } else {
